fa2f4baf592b54edc86832a5f084048a22ff0a77
IsaacGamero <isaac.gamero@utec.edu.pe>
2025-07-27 22:27:48 -0500

    Filling Menu

--- src/data/products.js
+++ src/data/products.js
@@ -1,22 +1,57 @@
 export const products = [
   // 🥢 Ramen
-  { id: 1, name: "Ramen Clásico", price: 24.90, category: "Ramen" },
-  { id: 2, name: "Udon", price: 26.90, category: "Ramen" },
+  { id: 1, name: "Udon (Sin picante)", price: 19.99, category: "Ramen", desc:"Caldo de Carne, panceta, huevo, algas" },
+  { id: 2, name: "Jin Mild (Picante Bajo)", price: 19.99, category: "Ramen", desc:"Caldo de Carne, panceta, huevo, algas"},
+  { id: 3, name: "Neoguri (Picante Bajo)", price: 19.99, category: "Ramen" , desc:"Caldo de pescado, panceta, huevo, narutos, algas"},
+  { id: 4, name: "Kimchi (Picante Medio)", price: 19.99, category: "Ramen", desc:"Caldo de a base de la verdura kimchi, panceta, huevo, algas" },
+  { id: 5, name: "Shin Ramen (Picante Medio)", price: 19.99, category: "Ramen", desc:"Caldo de Carne, panceta, huevo, algas"},
+  { id: 6, name: "Yeul (Picante Alto)", price: 19.99, category: "Ramen", desc:"Caldo de Carne, panceta, huevo, algas"},
+  
+  //Platos Secos
+  { id: 7, name: "Buldak Carbonara", price: 19.99, category: "Platos Secos", desc:"Fideos cremosos con 4 quesos, pollo y huevo montado" },
+  { id: 8, name: "Buldak Cheese", price: 19.99, category: "Platos Secos", desc:"Fideos cremosos en salsa de carbonara, pollo y huevo montado" },
+  { id: 9, name: "Jjajangmyeon", price: 19.99, category: "Platos Secos", desc:"Fideos con una salsa consistente de frijol negro, cebolla caramelizada y un toque dulce-salado" },
 
   // 🍶 Licores
-  { id: 3, name: "Soju Uva", price: 18.00, category: "Licores" },
+  { id: 10, name: "Soju Manzana", price: 18.00, category: "Licores" },
+  { id: 11, name: "Soju Frutos Rojos", price: 18.00, category: "Licores" },
+  { id: 12, name: "Soju Uva", price: 18.00, category: "Licores" },
+  { id: 13, name: "Soju Arandano", price: 18.00, category: "Licores" },
+  { id: 14, name: "Soju Citron", price: 18.00, category: "Licores" },
 
   // 🧃 Bebidas
-  { id: 4, name: "Baolisu Uva", price: 10.00, category: "Bebidas" },
+  { id: 15, name: "Baolisu Aloe Vera", price: 10.00, category: "Bebidas" },
+  { id: 16, name: "Baolisu Piña", price: 10.00, category: "Bebidas" },
+  { id: 17, name: "Baolisu Mango", price: 10.00, category: "Bebidas" },
+  { id: 18, name: "Baolisu Sandia", price: 10.00, category: "Bebidas" },
+  { id: 19, name: "Baolisu Uva", price: 10.00, category: "Bebidas" },
 
   // 🍡 Snacks
-  { id: 5, name: "Pepero Oreo", price: 10.00, category: "Snacks" },
-  { id: 6, name: "Chocopie Doble Chocolate", price: 3.00, category: "Snacks" },
-  { id: 7, name: "Chocopie Original", price: 3.00, category: "Snacks" },
-  { id: 8, name: "Galleta de la fortuna", price: 3.00, category: "Snacks" },
+  { id: 20, name: "Pepero Original", price: 10.00, category: "Snacks" },
+  { id: 21, name: "Pepero Oreo", price: 10.00, category: "Snacks" },
+  { id: 22, name: "Pepero Fresa", price: 10.00, category: "Snacks" },
+  { id: 23, name: "Pepero Almendra con Chocolate", price: 10.00, category: "Snacks" },
+  { id: 24, name: "Pepero Almendra con Chocolate Blanco", price: 10.00, category: "Snacks" },
+  { id: 25, name: "Pepero Relleno de Chocolate", price: 10.00, category: "Snacks" },
+  { id: 26, name: "Chocopie Doble Chocolate", price: 3.00, category: "Snacks" },
+  { id: 27, name: "Chocopie Original", price: 3.00, category: "Snacks" },
+  { id: 28, name: "Galleta de la fortuna", price: 3.00, category: "Snacks" },
 
   // 🧀 Toppings
-  { id: 9, name: "Queso Derretido + Salchicha", price: 2.50, category: "Toppings" },
+  { id: 29, name: "Porcion extra de Fideo", price: 5.00, category: "Toppings" },
+  { id: 30, name: "Queso Derretido + Salchicha", price: 2.50, category: "Toppings" },
+
+  //Milkis
+  { id: 31, name: "Milki Original", price: 8.00, category: "Bebidas" },
+  { id: 32, name: "Milki Zero", price: 8.00, category: "Bebidas" },
+  { id: 33, name: "Milki Fresa", price: 8.00, category: "Bebidas"},
+  { id: 34, name: "Milki Melon", price: 8.00, category: "Bebidas" },
+  { id: 35, name: "Milki Uva", price: 8.00, category: "Bebidas" },
+  { id: 36, name: "Milki Banana", price: 8.00, category: "Bebidas" },
+  { id: 37, name: "Milki Manzana", price: 8.00, category: "Bebidas"  },
+
+  //Houssy (Terminar)
+
 
   // 🎉 Promociones (para Home)
   { id: 101, name: "Combo Chaufa", price: 19.90, category: "Promociones", img:"chaufa.png", desc:"Arroz chaufa + bebida" },
